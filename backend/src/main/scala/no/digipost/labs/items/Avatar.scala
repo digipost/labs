f2edd383205f97c94e13e05112e5ee069aaf1b62
Øyvind Nerbråten <oyvind.nerbraten@bekk.no>
2016-06-14 09:03:19 +0200

    Fixed a bug where leading zeroes in the gravatar hash was removed

--- backend/src/main/scala/no/digipost/labs/items/Avatar.scala
+++ backend/src/main/scala/no/digipost/labs/items/Avatar.scala
@@ -1,14 +1,22 @@
 package no.digipost.labs.items
 
-import java.math.BigInteger
 import java.security.MessageDigest
 
 object Avatar {
+
   def emailToAvatar(email: Option[String], name: String): String = {
     val force = email.isEmpty
     val input = email.getOrElse(name)
-    val hash = new BigInteger(1, MessageDigest.getInstance("MD5").digest(input.trim.toLowerCase.getBytes)).toString(16)
+    val hash = toHex(toMd5(input.trim.toLowerCase))
     val avatar = s"https://secure.gravatar.com/avatar/$hash?d=identicon"
     if(force) s"$avatar&f=y" else avatar
+  }
+
+  private def toMd5(message: String): Array[Byte] = MessageDigest.getInstance("MD5").digest(message.getBytes())
+
+  private def toHex(byteArray : Array[Byte]): String = {
+    val sb = new StringBuffer()
+    byteArray.foreach(b => sb.append(Integer.toHexString((b & 0xFF) | 0x100).substring(1,3)))
+    sb.toString
   }
 }
